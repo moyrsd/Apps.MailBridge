ee7324f90e5eb8dd6af9a0ce8bd2e70075e6c0cf
moyrsd <me22b120@smail.iitm.ac.in>
2025-03-27 21:43:49 +0530

    llm, mail, oauth works together but separately

--- App/MailBridgeApp.ts
+++ App/MailBridgeApp.ts
@@ -1,17 +1,40 @@
-import {
-    IAppAccessors,
-    ILogger,
-    IConfigurationExtend,
-} from "@rocket.chat/apps-engine/definition/accessors";
+import { IConfigurationExtend } from "@rocket.chat/apps-engine/definition/accessors";
+// New files to be created in the project root folder
+import { OAuth2Service } from "./src/helpers/OAuth2Service";
+import { OAuthCommand } from "./src/commands/OauthCommand";
 import { App } from "@rocket.chat/apps-engine/definition/App";
-import { IAppInfo } from "@rocket.chat/apps-engine/definition/metadata";
 import { MailBridgeCommand } from "./src/commands/MailBridgeCommand";
 
-export class MailBridgeApp extends App {
-    constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
-        super(info, logger, accessors);
-    }
-    public async extendConfiguration(configuration: IConfigurationExtend) {
-        configuration.slashCommands.provideSlashCommand(new MailBridgeCommand());
+export class OAuthApp extends App {
+    private oauth2Service: OAuth2Service;
+
+    protected async extendConfiguration(
+        configuration: IConfigurationExtend
+    ): Promise<void> {
+        const oauthConfig = {
+            alias: "test",
+            accessTokenUri: "https://oauth2.googleapis.com/token",
+            authUri: "https://accounts.google.com/o/oauth2/v2/auth",
+            refreshTokenUri: "https://oauth2.googleapis.com/token",
+            revokeTokenUri: "https://oauth2.googleapis.com/revoke",
+            defaultScopes: [
+                "profile",
+                "email",
+                "https://www.googleapis.com/auth/gmail.readonly",
+                "https://www.googleapis.com/auth/gmail.send",
+                "https://www.googleapis.com/auth/gmail.compose",
+            ],
+        };
+
+        this.oauth2Service = new OAuth2Service(this, oauthConfig);
+        await this.oauth2Service.setup(configuration);
+
+        // Register the slash command and pass the logger
+        configuration.slashCommands.provideSlashCommand(
+            new OAuthCommand(this.oauth2Service, this.getLogger())
+        );
+        configuration.slashCommands.provideSlashCommand(
+            new MailBridgeCommand()
+        );
     }
 }
